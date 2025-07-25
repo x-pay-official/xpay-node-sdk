42e1eb7ca567841e7e1fc8c5234a8155152efa85
yan-notebook <1987.yanning@gmail.com>
2025-07-25 17:06:54 +0800

    initial

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * X-Pay Node.js SDK
+ * 
+ * Official SDK for integrating with the X-Pay cryptocurrency payment gateway
+ * 
+ * @packageDocumentation
+ */
+
+export * from './xpay';
+export * from './types';
+export * from './api-client';
